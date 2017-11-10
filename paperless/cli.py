79df252fa0e4665ac02a2c831b54388d95d1ab49
John D. Fisher <jdfenw@gmail.com>
2017-11-10 11:36:29 -0800

    Replace missed input with xml_input
    Correct output variable to use xml_input for deriving file name.
    
    Add typehints to cli.py

--- paperless/cli.py
+++ paperless/cli.py
@@ -2,18 +2,19 @@
 
 """Console script for paperless."""
 
+from pathlib import Path
 import click
 from paperless import convert, read_paperless, read_csv, write_csv
-from pathlib import Path
 
 
 @click.command()
-@click.option('--output', default = None, help = 'Todoist CSV task file')
-@click.option('--template', default = None, help = 'Todoist exported template')
-@click.option('--drop_duplicates', default = True,
-              help = 'Remove duplicate Paperless tasks')
+@click.option('--output', default=None, help='Todoist CSV task file')
+@click.option('--template', default=None, help='Todoist exported template')
+@click.option('--drop_duplicates', default=True,
+              help='Remove duplicate Paperless tasks')
 @click.argument('xml_input')
-def main(xml_input, output, template, drop_duplicates):
+def main(xml_input: str, output: str, template: str,
+         drop_duplicates: bool) -> None:
     """Console script for paperless."""
 
     df = read_paperless(xml_input, drop_duplicates=drop_duplicates)
@@ -24,7 +25,7 @@ def main(xml_input, output, template, drop_duplicates):
         dfnew = convert(df, columns=read_csv(template=template))
 
     if output is None:
-        output = Path(input).stem + '.csv'
+        output = Path(xml_input).stem + '.csv'
 
     write_csv(dfnew, output)
 
